c44eaea60cb86dd493acf60ac00fda21aba6d618
jmespitia <jmespitia@gmail.com>
2022-09-22 22:36:04 -0500

    Version 1

---
+++ src/main/java/com/example/WebApp/controller.java
@@ -0,0 +1,14 @@
+package com.example.WebApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/")
+    public String Mensaje() {
+        System.out.println("Mensaje de Exito");
+        return "Hola, Esta es una prueba con Spring Boot local";
+    }
+    
+}
